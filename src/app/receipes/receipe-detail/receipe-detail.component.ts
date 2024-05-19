ce957cdb176c3943b9b22069f49b737f5a0784e1
Michael Palmer <gitkraken.stoppage878@passmail.com>
2024-05-19 10:51:45 -0700

    Init of Angular App.

---
+++ src/app/receipes/receipe-detail/receipe-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-receipe-detail',
+  templateUrl: './receipe-detail.component.html',
+  styleUrl: './receipe-detail.component.css'
+})
+export class ReceipeDetailComponent {
+
+}
---
+++ src/app/receipes/receipe-list/receipe-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-receipe-list',
+  templateUrl: './receipe-list.component.html',
+  styleUrl: './receipe-list.component.css'
+})
+export class ReceipeListComponent {
+
+}
